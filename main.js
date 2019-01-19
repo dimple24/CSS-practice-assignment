71f31e0389c527f75bd802a8302c15243de56eca
dimple24 <32301107+dimple24@users.noreply.github.com>
2019-01-19 23:28:46 +0530

    added documentation

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
+// script for the preload page
 setTimeout(function() {
     document.getElementsByTagName("body")[0].classList.remove("no-scroll");
     document.getElementsByClassName("spinner-ctn")[0].style.display="none";
 },3000);
 
+// timeout function for landing page
 setTimeout(function() {
     document.getElementsByClassName("text")[0].style.display="block";
 }, 4000);
